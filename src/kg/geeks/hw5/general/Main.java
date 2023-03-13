23cfdba971658f80860c647af35fdb11dccc2952
KilluaZ12 <kamalmusaev.12.92@gmail.com>
2023-03-13 22:14:51 +0600

    Created boss class and hero class, added a method createHeroes

--- src/kg/geeks/hw5/general/Main.java
+++ src/kg/geeks/hw5/general/Main.java
@@ -1,5 +1,34 @@
+package kg.geeks.hw5.general;
+
+import kg.geeks.hw5.objects.Boss;
+import kg.geeks.hw5.objects.Hero;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Boss thanos = new Boss();
+        thanos.setHealth(1000);
+        thanos.setDamage(50);
+        thanos.setDefenseType("Armor");
+
+        System.out.println("Thanos health: " + thanos.getHealth() + ", his damage: " + thanos.getDamage()
+                + ", his type defense: " + thanos.getDefenseType());
+
+        Hero[] heroes = createHeroes();
+        for (Hero hero : heroes) {
+            System.out.println("Hero health: " + hero.getHealth());
+            System.out.println("Hero damage: " + hero.getDamage());
+            System.out.println("Hero superpower: " + hero.getSuperpower());
+        }
+
+
+    }
+
+    public static Hero[] createHeroes(){
+        Hero scarletWitch = new Hero(100, 20, "Flight");
+        Hero aurora = new Hero(80, 30, "Invisibility");
+        Hero warrior = new Hero(120, 25);
+
+        return new Hero[] {scarletWitch, aurora, warrior};
     }
 }
--- src/kg/geeks/hw5/objects/Boss.java
+++ src/kg/geeks/hw5/objects/Boss.java
@@ -1,2 +1,41 @@
-package kg.geeks.hw5.objects;public class Boss {
+package kg.geeks.hw5.objects;
+
+public class Boss {
+    private int health;
+    private int damage;
+    private String defenseType;
+
+    public Boss(){}
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        if (health <= 0) {
+            System.out.println("Wrong value for health, it must be positive number!");
+        } else {
+            this.health = health;
+        }
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        if (damage <= 0) {
+            System.out.println("Wrong value for damage, it must be positive number!");
+        } else {
+            this.damage = damage;
+        }
+    }
+
+    public String getDefenseType() {
+        return defenseType;
+    }
+
+    public void setDefenseType(String defenseType) {
+        this.defenseType = defenseType;
+    }
 }
